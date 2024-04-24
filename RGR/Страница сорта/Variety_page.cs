20cf48d15d53466303f5cbe90087d9a8b5de3b0e
Евгений <evgeniy.martsin@mail.ru>
2024-04-24 19:12:36 +0300

    Add class "Export" and add it in VarietyPage export

--- RGR/Страница сорта/Variety_page.cs
+++ RGR/Страница сорта/Variety_page.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using RGR.Страница_сорта;
 
 namespace RGR
 {
@@ -266,27 +267,8 @@ namespace RGR
         {
             if (dataTable.Rows.Count > 0)
             {
-                Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
-                word.Application.Documents.Add(Type.Missing);
-
-                Microsoft.Office.Interop.Word.Table table = word.Application.ActiveDocument.Tables.Add(word.Selection.Range, dataTable.Rows.Count + 1, dataTable.Columns.Count, Type.Missing, Type.Missing);
-                table.Borders.OutsideLineStyle = Microsoft.Office.Interop.Word.WdLineStyle.wdLineStyleSingle;
-                table.Borders.InsideLineStyle = Microsoft.Office.Interop.Word.WdLineStyle.wdLineStyleSingle;
-
-                for (int i = 0; i < dataTable.Columns.Count; i++)
-                {
-                    table.Cell(1, i + 1).Range.Text = dataTable.Columns[i].ColumnName;
-                }
-
-                for (int i = 0; i < dataTable.Rows.Count; i++)
-                {
-                    for (int j = 0; j < dataTable.Columns.Count; j++)
-                    {
-                        table.Cell(i + 2, j + 1).Range.Text = dataTable.Rows[i][j].ToString();
-                    }
-                }
-
-                word.Visible = true;
+                Export word = new Export(dataTable);
+                word.createWord();
             }
             else
             {
